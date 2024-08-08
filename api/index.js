51fe05572f89978125caadc4ee718c786d3c28da
Nissam <nisamm224@gmail.com>
2024-08-08 06:30:11 +0530

    Mongoose installed, Implemented some Configs

--- api/index.js
+++ api/index.js
@@ -1,11 +1,19 @@
 import express from 'express';
-import userRoutes from './route/user.route.js'
+import userRoutes from './routes/user.route.js'
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
 const app = express();
 const port = 3000;
+dotenv.config();
+app.use(express.json());
 
 app.listen(port, () => {
     console.log(`Server running on port ${3000}`);
 })
 
-app.use('/user', userRoutes);
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log('MongoDb Connected');
+});
+
+app.use('/api', userRoutes);
